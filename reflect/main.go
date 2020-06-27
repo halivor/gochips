510468542d3c370e84c662c7efc7578a3e4f220e
halivor <hongliang186@aliyun.com>
2020-06-27 18:58:21 +0800

    调度reflect

--- reflect/main.go
+++ reflect/main.go
@@ -5,7 +5,37 @@ import (
 	"reflect"
 )
 
+type img struct {
+	Image string
+}
+
 func main() {
+	is := []*img{&img{"a"}, &img{"b"}, &img{"c"}}
+	getImage(is)
+}
+
+func getImage(is interface{}) {
+	if reflect.ValueOf(is).Kind() != reflect.Slice {
+		return
+	}
+	slen := reflect.ValueOf(is).Len()
+	for i := 0; i < slen; i++ {
+		isd, _ := reflect.Indirect(reflect.ValueOf(is).Index(i)).
+			FieldByName("Image").Interface().(string)
+		fmt.Println(reflect.ValueOf(is).Index(i), isd)
+	}
+	return
+}
+
+func pValue() {
+	i := &img{"d"}
+	T := reflect.TypeOf(i)
+	V := reflect.ValueOf(i)
+
+	fmt.Println(T, V, reflect.Indirect(V).FieldByName("Image").Interface())
+}
+
+func call() {
 	m := New()
 
 	mm := map[string]interface{}{"method": m}
